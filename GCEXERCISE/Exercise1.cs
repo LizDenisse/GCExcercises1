6d127c85c568296cc1b4dc11f9936a52bea1cecf
LizHitches <liz.hitches@gmail.com>
2019-07-08 21:46:37 -0400

    excerices commit

---
+++ GCEXERCISE/Exercise1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Inclasslabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Some Text");
+            string text = Console.ReadLine();
+
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
+/*
+ * DescriptionPrompt the user to enter a string. After the user enters a string, 
+ * output the same string back tothe console.
+ * Example>>Enter some text: <<Hello, World! ECHOOOOOO!>>Hello, World! ECHOOOOOO!
+ */
